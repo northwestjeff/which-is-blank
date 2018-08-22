cfca16893b2b34784e47e11ca211a39a85e39e27
northwestjeff <northwestjeff@gmail.com>
2018-08-22 12:02:23 -0700

    connecting js to html

---
+++ whichisblank/static/whichisblank/scripts/script.js
@@ -0,0 +1,5 @@
+
+function colorOnClick(id) {
+  console.log("click")
+  document.getElementById('id').style.colorOnHover = "#ff2200"
+}
---
+++ whichisblank/whichisblank/static/whichisblank/scripts/script.js
@@ -0,0 +1,11 @@
+
+function colorOnClick(id) {
+  console.log("click")
+  document.getElementById('id').style.colorOnHover = "#ff2200"
+}
+
+
+function myFunction() {
+    console.log('click');
+
+}
